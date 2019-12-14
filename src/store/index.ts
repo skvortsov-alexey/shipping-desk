ba64c0d2d8d9e96d6aae5041a12108b9cd8703ac
skvortsov-alexey <skv.an.89@gmail.com>
2019-12-15 01:38:32 +0600

    Add react-redux. Connect app to redux. Set up redux devttool, hot reload

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,6 +2,17 @@ import { createStore } from 'redux'
 
 import rootReducer from './reducer'
 
-const store = createStore(rootReducer)
+const store = createStore(
+  rootReducer,
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
+    (window as any).__REDUX_DEVTOOLS_EXTENSION__()
+)
+
+if (process.env.NODE_ENV === 'development' && module.hot) {
+  module.hot.accept('./reducer', () => {
+    const newRootReducer = require('./reducer').default
+    store.replaceReducer(newRootReducer)
+  })
+}
 
 export default store
