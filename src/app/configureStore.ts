8f6e41e2d3f62c76e182e4824796912599a8d2f7
skvortsov-alexey <skv.an.89@gmail.com>
2019-12-18 02:44:16 +0600

    Add redux-saga. Connect sagaMiddleware to redux store. Create rootSaga.

--- src/app/configureStore.ts
+++ src/app/configureStore.ts
@@ -1,12 +1,19 @@
-import { createStore } from 'redux'
+import { createStore, applyMiddleware, compose } from 'redux'
+import createSagaMiddleware from 'redux-saga'
+
 import rootReducer from './reducer'
+import rootSaga from './sagas'
 
 function configureStore() {  
-  const store = createStore(
-    rootReducer,
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ &&
-      (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  const composeEnhancers = (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  
+  const sagaMiddleware = createSagaMiddleware()
+  const middlewares = [sagaMiddleware]
+  const enhancers = [applyMiddleware(...middlewares)]
+
+  const store = createStore(rootReducer, composeEnhancers(...enhancers))
+
+  sagaMiddleware.run(rootSaga)
   
   if (process.env.NODE_ENV === 'development' && module.hot) {
     module.hot.accept('./reducer', () => {
